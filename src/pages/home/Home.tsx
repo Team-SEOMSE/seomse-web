3378a3782ddfa6b3809737f4c5a0e7810a7eb9c8
Sojin <118108221+kimSojinn@users.noreply.github.com>
2025-08-21 00:01:42 +0900

    feat: 홈 화면 > 컨텐츠 UI 작업

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,16 @@
+import Banner from "../../components/home/Banner";
 import Header from "../../layout/header/Header";
 import Navbar from "../../layout/navbar/Navbar";
 import styles from "./Home.module.css";
 
 const Home = () => {
-    return (
-        <div className={styles.screen}>
-            <Header />
-
-            <Navbar />
-        </div>
-    );
+  return (
+    <div className={styles.screen}>
+      <Header />
+      <Banner />
+      <Navbar />
+    </div>
+  );
 };
 
 export default Home;
