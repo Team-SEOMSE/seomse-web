51f66f0e9fcdc390ef6bfb4e4e00cfbaf0861fa7
Sojin <118108221+kimSojinn@users.noreply.github.com>
2025-08-20 23:23:33 +0900

    feat: 마이페이지 UI 완성 & 헤더 간략화

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,13 +1,13 @@
 import styles from "./Login.module.css";
 
 const Login = () => {
-    return (
-        <div className={styles.login_section}>
-            <h1 className={styles.login_title}>로그인</h1>
-            <p>3초 카카오 로그인</p>
-            <p>일반 로그인 | 일반 회원가입</p>
-        </div>
-    );
+  return (
+    <div className={styles.login_section}>
+      <h1 className={styles.login_title}>로그인</h1>
+      <p className={styles.text}>3초 카카오 로그인</p>
+      <p className={styles.text}>일반 로그인 | 일반 회원가입</p>
+    </div>
+  );
 };
 
 export default Login;
