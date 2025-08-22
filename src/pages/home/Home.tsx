d6a41b65964e86b0c0ac966071a31746ca98086f
Sojin <118108221+kimSojinn@users.noreply.github.com>
2025-08-22 10:51:55 +0900

    feat: Home 화면 속 컴포넌트 작업
    - 상단 Banner 컴포넌트 작업 완료 (이어서 클릭시 알림 연결 작업 진행 예정)
    - Bubble 컴포넌트 또한 api 연결 후 연령대, 이름 받아와서 동적값 보여줄 예정
    - SectionTitle 공통 컴포넌트 작업 완료

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,21 @@
+import SectionTitle from "../../components/common/sectionTitle/SectionTitle";
 import Banner from "../../components/home/Banner";
+import Bubble from "../../components/home/Bubble";
 import Header from "../../layout/header/Header";
 import Navbar from "../../layout/navbar/Navbar";
 import styles from "./Home.module.css";
 
 const Home = () => {
-  return (
-    <div className={styles.screen}>
-      <Header />
-      <Banner />
-      <Navbar />
-    </div>
-  );
+    return (
+        <div className={styles.screen}>
+            <Header />
+            <Banner />
+            <Bubble ageGroup="20대" userName="김섬세" />
+            <SectionTitle>AI 개인화 추천</SectionTitle>
+            <SectionTitle>우리 동네 인기 헤어샵</SectionTitle>
+            <Navbar />
+        </div>
+    );
 };
 
 export default Home;
