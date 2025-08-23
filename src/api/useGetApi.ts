cb8f64609e8d121ce591ee4433f13f5ec34be120
Sojin <118108221+kimSojinn@users.noreply.github.com>
2025-08-24 00:20:02 +0900

    feat: api 모듈 추가
    - api 개발 예정인 GET,POST,PATCH 메서드 요청 모듈화했습니다.
    - useCookie 훅은 토큰값 저장에 사용될 예정입니다.

---
+++ src/api/useGetApi.ts
@@ -0,0 +1,41 @@
+import { useQuery } from "@tanstack/react-query";
+import { API_URL } from "./URL";
+import { getCookie } from "../hooks/useCookie";
+
+const useGetApi = (key: string, url: string, queryOptions?: object) => {
+  const COMPOUND_URL = `${API_URL}${url}`;
+
+  const token = getCookie("accessToken");
+
+  const options: RequestInit = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
+    },
+    credentials: "include",
+  };
+
+  const getDatas = async () => {
+    try {
+      const res = await fetch(COMPOUND_URL, options);
+      if (!res.ok) {
+        const errorResponse = await res.json();
+        throw new Error(errorResponse.message);
+      }
+      return await res.json();
+    } catch (error) {
+      console.error("[useGetApi] fetch error:", error);
+    }
+  };
+
+  const { refetch, data, isLoading, isError, error } = useQuery({
+    queryKey: [key, url],
+    queryFn: getDatas,
+    ...queryOptions,
+  });
+
+  return { data, isLoading, isError, error, refetch };
+};
+
+export default useGetApi;
---
+++ src/api/usePatch.ts
@@ -0,0 +1,45 @@
+import { useMutation } from "@tanstack/react-query";
+import { API_URL } from "../api/URL";
+import { getCookie } from "../hooks/useCookie";
+
+const usePatchApi = (key: string, url: string) => {
+  const COMPOUND_URL = `${API_URL}${url}`;
+  const token = getCookie("accessToken");
+
+  const patchDatas = async (body: Record<string, unknown>) => {
+    const options = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        ...(token ? { Authorization: `Bearer ${token}` } : {}),
+      },
+      body: JSON.stringify(body),
+    };
+
+    const res = await fetch(COMPOUND_URL, options);
+
+    if (!res.ok) {
+      const errorResponse = await res.json();
+      throw new Error(errorResponse.message);
+    }
+
+    const data = await res.json();
+
+    return data;
+  };
+
+  const { mutate, data, isPending, isError, error } = useMutation({
+    mutationKey: [key],
+    mutationFn: patchDatas,
+    onSuccess: () => {
+      console.log("요청 성공");
+    },
+    onError: () => {
+      console.log(`요청 실패: ${error}`);
+    },
+  });
+
+  return { data, isPending, isError, error, mutate };
+};
+
+export default usePatchApi;
