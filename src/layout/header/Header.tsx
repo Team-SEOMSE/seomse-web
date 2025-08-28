5223a6e5cb191ce866a9a534daadbdd32a12f4a8
Sojin <118108221+kimSojinn@users.noreply.github.com>
2025-08-28 14:57:47 +0900

    style: navbar & header 디자인 반영
    - 레이아웃 새 디자인 반영했습니다.
    - 회원가입시 아이디 중복값 확인 버튼 추가했습니다.
    - 파비콘 교체했습니다.

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,20 +1,17 @@
-// import Search from "../../assets/svg/search.svg";
-// 모바일로 로고 화질 확인해볼 것!
-import Logo from "../../assets/svg/svgHeader.svg";
-
+import Logo from "../../assets/images/headerLogo.png";
 import styles from "./Header.module.css";
 
 const Header = () => {
-  return (
-    <header className={styles.header}>
-      <img className={styles.logo} src={Logo} alt="섬세 메인 로고" />
-      {/* <img
+    return (
+        <header className={styles.header}>
+            <img className={styles.logo} src={Logo} alt="섬세 메인 로고" />
+            {/* <img
                 className={styles.search_icon}
                 src={Search}
                 alt="검색 아이콘"
             /> */}
-    </header>
-  );
+        </header>
+    );
 };
 
 export default Header;
