08627171f342eb997e38e4b5d4713f508b128153
Sojin <118108221+kimSojinn@users.noreply.github.com>
2025-08-30 22:34:16 +0900

    style: 로고 화질 개선 & navbar 높이 조정
    - 모바일 시현 후 보인 문제점 1차 개선

--- src/components/login/KakaoLoginSection.tsx
+++ src/components/login/KakaoLoginSection.tsx
@@ -1,52 +1,42 @@
 import { useNavigate } from "react-router-dom";
-import Logo from "../../assets/images/mainLogo.png";
+import Logo from "../../assets/svg/mainLogo.svg";
 import { ReactComponent as KakaoIcon } from "../../assets/svg/kakao.svg";
 import type { ButtonElements, ButtonStyles } from "../../types/common/button";
 import Button from "../common/button/Button";
 import styles from "./KakaoLoginSection.module.css";
 
 const STYLE: ButtonStyles = {
-    color: "#000000",
-    backgroundColor: "#fee500",
+  color: "#000000",
+  backgroundColor: "#fee500",
 };
 
 const KakaoLoginSection = () => {
-    const navigate = useNavigate();
-    const buttonElements: ButtonElements = {
-        content: "카카오로 로그인",
-        handleClick: () => {
-            // 카카오 로그인 개발 전 임시로 페이지 이동
-            navigate("/user-details");
-        },
-    };
+  const navigate = useNavigate();
+  const buttonElements: ButtonElements = {
+    content: "카카오로 로그인",
+    handleClick: () => {
+      // 카카오 로그인 개발 전 임시로 페이지 이동
+      navigate("/user-details");
+    },
+  };
 
-    return (
-        <div className={styles.login_section}>
-            <img className={styles.logo} src={Logo} alt="섬세 메인 로고" />
-            <div className={styles.login_box}>
-                <Button
-                    elements={buttonElements}
-                    style={STYLE}
-                    icon={<KakaoIcon />}
-                />
-                <div className={styles.text_wrapper}>
-                    <p
-                        className={styles.text}
-                        onClick={() => navigate("/email-login")}
-                    >
-                        이메일로 로그인
-                    </p>
-                    |
-                    <p
-                        className={styles.text}
-                        onClick={() => navigate("/signup")}
-                    >
-                        이메일로 회원가입
-                    </p>
-                </div>
-            </div>
+  return (
+    <div className={styles.login_section}>
+      <img className={styles.logo} src={Logo} alt="섬세 메인 로고" />
+      <div className={styles.login_box}>
+        <Button elements={buttonElements} style={STYLE} icon={<KakaoIcon />} />
+        <div className={styles.text_wrapper}>
+          <p className={styles.text} onClick={() => navigate("/email-login")}>
+            이메일로 로그인
+          </p>
+          |
+          <p className={styles.text} onClick={() => navigate("/signup")}>
+            이메일로 회원가입
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default KakaoLoginSection;
--- src/pages/splash/Splash.tsx
+++ src/pages/splash/Splash.tsx
@@ -1,25 +1,25 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Logo from "../../assets/images/mainLogo.png";
+import Logo from "../../assets/svg/mainLogo.svg";
 import styles from "./Splash.module.css";
 
 const Splash = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            navigate("/kakao-login");
-        }, 2200);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/kakao-login");
+    }, 2200);
 
-        return () => clearTimeout(timer);
-    }, [navigate]);
+    return () => clearTimeout(timer);
+  }, [navigate]);
 
-    return (
-        <div className={styles.screen}>
-            <img className={styles.logo} src={Logo} alt="섬세 메인 로고" />
-            <p className={styles.corp}>© Seomse corp.</p>
-        </div>
-    );
+  return (
+    <div className={styles.screen}>
+      <img className={styles.logo} src={Logo} alt="섬세 메인 로고" />
+      <p className={styles.corp}>© Seomse corp.</p>
+    </div>
+  );
 };
 
 export default Splash;
