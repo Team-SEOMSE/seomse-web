f4cce9c1d75f49d42b0b0165d8226d488cc3883a
HOYA <als77770@gmail.com>
2025-09-02 01:21:09 +0900

    feat: vite-plugin-checker 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import svgr from "@svgr/rollup";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 
 export default defineConfig({
-    plugins: [svgr(), react()],
+  plugins: [
+    svgr(),
+    react(),
+    checker({
+      typescript: true,
+    }),
+  ],
 });
