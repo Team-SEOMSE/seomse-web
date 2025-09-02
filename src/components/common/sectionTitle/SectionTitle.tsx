cc6b8d5cc3bd33213bedafcf91129a428ca95b7c
HOYA <als77770@gmail.com>
2025-09-03 02:33:02 +0900

    style: 홈 화면에 말풍선 추가

--- src/components/common/sectionTitle/SectionTitle.tsx
+++ src/components/common/sectionTitle/SectionTitle.tsx
@@ -3,12 +3,14 @@ import styles from "./SectionTitle.module.css";
 type SectionTitleProps = {
     children: React.ReactNode;
     icon?: React.ReactNode;
+    sideSlot?: React.ReactNode;
 };
 
-const SectionTitle = ({ children, icon }: SectionTitleProps) => (
+const SectionTitle = ({ children, icon, sideSlot }: SectionTitleProps) => (
     <h2 className={styles.title}>
         {icon && <span className={styles.icon}>{icon}</span>}
         <span className={styles.text}>{children}</span>
+        {sideSlot}
     </h2>
 );
 
