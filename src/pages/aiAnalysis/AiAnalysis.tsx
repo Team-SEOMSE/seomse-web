931a6b74a725d7524316a314d9c3392300bfe0c4
Sojin <118108221+kimSojinn@users.noreply.github.com>
2025-11-24 17:14:14 +0900

    feat: 기능 개발 전 임시 UI 추가

--- src/pages/aiAnalysis/AiAnalysis.tsx
+++ src/pages/aiAnalysis/AiAnalysis.tsx
@@ -6,7 +6,12 @@ const AiAnalysis = () => {
     return (
         <div className={styles.screen}>
             <Header />
-            <div className={styles.content}></div>
+            <div className={styles.content}>
+                <p className={styles.notice}>
+                    AI 분석 기능을 준비 중입니다.
+                    <br /> 곧 업데이트될 예정입니다!
+                </p>
+            </div>
             <Navbar />
         </div>
     );
